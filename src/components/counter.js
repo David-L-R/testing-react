502c3ff04ab64cad2487e04607e7c7c4f27b6596
David Rajcher (Codaisseur) <davidrajchercodaisseur@Davids-MacBook-Pro.local>
2022-05-12 19:10:45 +0200

    init

--- src/components/counter.js
+++ src/components/counter.js
@@ -3,14 +3,11 @@
 import * as React from 'react'
 
 function Counter() {
-  const [count, setCount] = React.useState(0)
-  const increment = () => setCount(c => c + 1)
-  const decrement = () => setCount(c => c - 1)
   return (
     <div>
-      <div>Current count: {count}</div>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={increment}>Increment</button>
+      <div>Current count: </div>
+      <button>Decrement</button>
+      <button>Increment</button>
     </div>
   )
 }
